f4aac0df8054109c48e35cbb09f377d6abadcc45
Colin Spence <udappia@gmail.com>
2022-07-06 15:37:37 -0600

    checkpoint

--- apps/minting-app/components/ColorPicker.tsx
+++ apps/minting-app/components/ColorPicker.tsx
@@ -1,18 +1,31 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import classes from "./ColorPicker.module.css";
 
 export const Canvas = () => {
   const ref = useRef<HTMLCanvasElement | null>(null);
   const canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
   let ctx = canvasCtxRef.current;
 
+  // State
+  // let [hueValue, setHueValue] = useState({ x: 0, y: 0 });
+  // let [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  // let [elementPosition, setElementPosition] = useState({x: circleOffset.left, y: circleOffset.top})
+
   // ----- CONFIG ----- //
 
   // Common
   const size = 200;
   const thickness = size / 20;
+  const knobRadius = 15;
+  // let pickerElement = document.querySelector(".circlePicker")
+  // let circleOffset = {top: pickerElement?.offsetTop, left: pickerElement?.offsetLeft};
+  let mouseDown = false;
+  let knobType;
 
   // Hue
   const hueRadius = size / 2;
+  let hueTarget = 180;
+  let hueKnobPos = { x: 0, y: 0 };
 
   // Lightness
   const lightnessSize = size * 0.75;
@@ -20,8 +33,42 @@ export const Canvas = () => {
   const lightnessRange = [80, 40]; // Max lightness, Min lightness
 
   const updateSlider = () => {
+    setHueKnobPosition();
     drawHueArc();
     drawLightnessArc();
+  };
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    console.log(e.target.id);
+    mouseDown = true;
+    knobType = e.target.id;
+  };
+
+  const handleMouseUp = (e: React.MouseEvent) => {
+    mouseDown = false;
+  };
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (mouseDown) {
+      if (knobType === "hue") {
+        // setHueValue();
+      } else {
+        // setLightnessPosition(event);
+      }
+    }
+  };
+
+  const setHueKnobPosition = () => {
+    hueKnobPos.x =
+      Math.round(hueRadius * Math.sin((hueTarget * Math.PI) / 180)) +
+      hueRadius -
+      knobRadius +
+      4;
+    hueKnobPos.y =
+      Math.round(hueRadius * -Math.cos((hueTarget * Math.PI) / 180)) +
+      hueRadius -
+      knobRadius +
+      4;
   };
 
   const drawHueArc = () => {
@@ -71,16 +118,60 @@ export const Canvas = () => {
     ctx!.restore();
   };
 
+  // const setHueValue = (e: React.MouseEvent) => {
+  //   let atan, hueTarget, val;
+  //   setMousePosition = {
+  //     x: e.pageX - elementPosition.x,
+  //     y: e.pageY - elementPosition.y
+  //   };
+  //   atan = Math.atan2(mousePosition.x - hueRadius, mousePosition.y - hueRadius);
+  //   hueTarget = -atan / (Math.PI / 180) + 180;
+
+  //   hueValue = hueTarget;
+
+  //   return hueValue
+  //   };
+  // }
+
   useEffect(() => {
     if (ref.current) {
       canvasCtxRef.current = ref.current.getContext("2d");
       ctx = canvasCtxRef.current;
+      document
+        .querySelector("body")
+        ?.addEventListener("mouseup", handleMouseUp);
 
       updateSlider();
     }
   }, []);
 
-  return <canvas ref={ref} width={size} height={size} />;
+  return (
+    <div className={classes.colorPicker} style={{ width: size, height: size }}>
+      <canvas ref={ref} width={size} height={size} />
+      <span className={classes.value}></span>
+      <div
+        id="hue"
+        className={classes.knob}
+        unselectable="on"
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        style={{
+          height: knobRadius * 2,
+          left: hueKnobPos.x,
+          width: knobRadius * 2,
+          top: hueKnobPos.y,
+        }}
+      />
+      <div
+        id="lightness"
+        className={classes.knob}
+        unselectable="on"
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        style={{ width: knobRadius * 2, height: knobRadius * 2 }}
+      />
+    </div>
+  );
 };
 
 export default React.memo(Canvas);
