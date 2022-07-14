9c4c46d7bd0dd4ef5d05292beac892f3c4e897bb
samepant <sampanter@gmail.com>
2022-07-14 17:04:19 -0400

    add bg ring to stone picker

--- apps/minting-app/components/stonePicker/index.tsx
+++ apps/minting-app/components/stonePicker/index.tsx
@@ -31,6 +31,44 @@ const StonePicker: React.FC = () => {
   return (
     <div className={styles.stoneBag}>
       <UiCircle>
+        <svg
+          viewBox="0 0 120 120"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={styles.bgCircle}
+        >
+          <defs>
+            <linearGradient
+              id="bg_grad"
+              x1="60"
+              y1="3.28491"
+              x2="60"
+              y2="118.027"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#EFEBCE" stopOpacity="0.5" />
+              <stop offset="1" stopColor="#EFEBCE" />
+            </linearGradient>
+          </defs>
+          <circle
+            cx="60"
+            cy="60"
+            r="52"
+            stroke="url(#bg_grad)"
+            strokeWidth="5"
+            opacity="1"
+            style={{ mixBlendMode: "color-dodge" }}
+          />
+          <circle
+            cx="60"
+            cy="60"
+            r="57"
+            stroke="url(#bg_grad)"
+            strokeWidth="2"
+            opacity="1"
+            style={{ mixBlendMode: "color-dodge" }}
+          />
+        </svg>
         <ul>
           {stonesWithPosition.map(({ id, position }) => (
             <DraggableNoType key={id} bounds="parent" nodeRef={nodeRef}>
