239cd057c29f940294bf5287aa51728a7d76dbcd
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-07-18 15:53:19 +0200

    make it kinda work

--- apps/minting-app/components/stonePicker/StoneViewer.tsx
+++ apps/minting-app/components/stonePicker/StoneViewer.tsx
@@ -90,47 +90,6 @@ const StoneViewer: React.FC<{
         fill="url(#ss)"
         clipPath="url(#picker_sc)"
       />
-      <defs>
-        <radialGradient
-          id="picker_sf"
-          cx="606.78"
-          cy="1003.98"
-          fx="606.78"
-          fy="1003.98"
-          r="2"
-          gradientTransform="translate(-187630.67 -88769.1) rotate(-33.42) scale(178.04 178.05)"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop offset=".05" stopColor="#fff" stopOpacity=".7" />
-          <stop offset=".26" stopColor="#ececec" stopOpacity=".5" />
-          <stop offset=".45" stopColor="#c4c4c4" stopOpacity=".5" />
-          <stop offset=".63" stopColor="#929292" stopOpacity=".5" />
-          <stop offset=".83" stopColor="#7b7b7b" stopOpacity=".5" />
-          <stop offset="1" stopColor="#cbcbca" stopOpacity=".5" />
-        </radialGradient>
-        <radialGradient
-          id="picker_sh"
-          cx="1149"
-          cy="2660"
-          fx="1149"
-          fy="2660"
-          r="76"
-          gradientTransform="translate(312 2546) rotate(-20) scale(1 -.5)"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop offset="0" stopColor="#fff" stopOpacity=".7" />
-          <stop offset="1" stopColor="#fff" stopOpacity="0" />
-        </radialGradient>
-      </defs>
-
-      <path
-        fill="url(#picker_sf)"
-        d="M1184 876a260 260 0 1 1-368 368 260 260 0 0 1 368-368Z"
-      />
-      <path
-        fill="url(#picker_sh)"
-        d="M919 857c49-20 96-15 107 11 10 26-21 62-70 82s-97 14-107-12c-10-25 21-62 70-81Z"
-      />
     </svg>
   );
 };
--- apps/minting-app/components/stonePicker/index.tsx
+++ apps/minting-app/components/stonePicker/index.tsx
@@ -8,27 +8,33 @@ import UiCircle from "../uiCircle";
 import { useAppContext } from "../../state";
 import IconButton from "../IconButton";
 import useInertia from "./useInertia";
+import { Stone } from "../../types";
+import StoneGlass from "./StoneGlass";
 
 const stoneCount = stoneList.length;
 
-const ANGLE_STEP = 5;
+const ANGLE_STEP = 45;
 
 const StonePicker: React.FC = () => {
   const { state, dispatch } = useAppContext();
   const [tempStone, setTempStone] = useState(state.stone);
+  const [angle, setAngle] = useState(state.stone * ANGLE_STEP);
   // const rotate = (360 / stoneCount) * state.stone;
 
-  const [{ rotation, turbFreqX }, set] = useInertia({
+  const [{ rotation }, set] = useInertia({
     rotation: ANGLE_STEP * tempStone,
-    turbFreqX: stoneList[tempStone].turbFreqX,
-    onChange(rotation: any) {
+    onChange(rotation: number) {
       if (isNaN(rotation)) return;
-      const i = Math.round(rotation / ANGLE_STEP) % stoneCount;
-      const stoneIndex = i < 0 ? stoneCount + i : i;
-      if (stoneIndex !== tempStone) {
-        setTempStone(stoneIndex);
-      }
+      setAngle(rotation);
     },
+    // onChange(rotation: any) {
+    //   if (isNaN(rotation)) return;
+    //   const i = Math.round(rotation / ANGLE_STEP) % stoneCount;
+    //   const stoneIndex = i < 0 ? stoneCount + i : i;
+    //   if (stoneIndex !== tempStone) {
+    //     setTempStone(stoneIndex);
+    //   }
+    // },
   });
 
   if (!rotation) throw new Error("huh");
@@ -81,16 +87,13 @@ const StonePicker: React.FC = () => {
       >
         <UiCircle>
           <div className={styles.stone}>
-            <StoneViewer
-              seed={state.tokenId}
-              stone={{
-                ...stoneList[tempStone],
-                turbFreqX: turbFreqX?.get() || stoneList[tempStone].turbFreqX,
-              }}
-            />
+            <StoneViewer seed={state.tokenId} stone={interpolateStone(angle)} />
           </div>
         </UiCircle>
       </animated.div>
+      <div className={styles.stone}>
+        <StoneGlass />
+      </div>
       <div className={styles.icon}>
         <IconButton icon="PickerStone" shadow />
       </div>
@@ -99,3 +102,42 @@ const StonePicker: React.FC = () => {
 };
 
 export default StonePicker;
+
+const interpolateStone = (angle: number): Stone => {
+  const i = Math.floor(angle / ANGLE_STEP) % stoneCount;
+  const fromIndex = i < 0 ? stoneCount + i : i;
+  const toIndex = (i + 1) % stoneCount;
+  const fromStone = stoneList[fromIndex];
+  const toStone = stoneList[toIndex];
+  const progress = (angle % ANGLE_STEP) / ANGLE_STEP;
+  return {
+    turbFreqX: interpolateValue(
+      fromStone.turbFreqX,
+      toStone.turbFreqX,
+      progress
+    ),
+    turbFreqY: interpolateValue(
+      fromStone.turbFreqY,
+      toStone.turbFreqY,
+      progress
+    ),
+    turbOct: Math.round(
+      interpolateValue(fromStone.turbOct, toStone.turbOct, progress)
+    ),
+    redAmp: interpolateValue(fromStone.redAmp, toStone.redAmp, progress),
+    redExp: interpolateValue(fromStone.redExp, toStone.redExp, progress),
+    redOff: interpolateValue(fromStone.redOff, toStone.redOff, progress),
+    greenAmp: interpolateValue(fromStone.greenAmp, toStone.greenAmp, progress),
+    greenExp: interpolateValue(fromStone.greenExp, toStone.greenExp, progress),
+    greenOff: interpolateValue(fromStone.greenOff, toStone.greenOff, progress),
+    blueAmp: interpolateValue(fromStone.blueAmp, toStone.blueAmp, progress),
+    blueExp: interpolateValue(fromStone.blueExp, toStone.blueExp, progress),
+    blueOff: interpolateValue(fromStone.blueOff, toStone.blueOff, progress),
+    fractalNoise:
+      progress < 0.5 ? fromStone.fractalNoise : toStone.fractalNoise,
+    rotation: interpolateValue(fromStone.rotation, toStone.rotation, progress),
+  };
+};
+
+const interpolateValue = (from: number, to: number, progress: number) =>
+  from + (to - from) * progress;
