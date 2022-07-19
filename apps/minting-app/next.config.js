9775e5374f7a8cf73a0d91d5156007760d294ab5
samepant <sampanter@gmail.com>
2022-07-19 10:56:40 -0400

    fix basePath for build

--- apps/minting-app/next.config.js
+++ apps/minting-app/next.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.BASE_PATH || "",
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
