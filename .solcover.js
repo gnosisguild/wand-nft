6239c62b23311cbf915fdba6919bab625b93000c
nginnever <ginneversource@gmail.com>
2022-07-25 23:51:24 -0700

    remove monorepo files

--- .solcover.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  skipFiles: ["test/Imports.sol", "test/TestAvatar.sol", "test/Mock.sol"],
-  mocha: {
-    grep: "@skip-on-coverage", // Find everything with this tag
-    invert: true, // Run the grep's inverse set.
-  },
-};
