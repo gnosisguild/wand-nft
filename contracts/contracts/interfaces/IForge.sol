c3722272566c2f9182444dca6f357a4a0a365383
nginnever <ginneversource@gmail.com>
2022-07-27 16:00:49 -0700

    begin tracking assigned xp

--- contracts/contracts/interfaces/IForge.sol
+++ contracts/contracts/interfaces/IForge.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.6;
 
 interface IForge {
+  struct Character {
+    uint256 XP;
+    uint256 XPAssigned;
+  }
+
   function getILvl(uint256 tokenId)
     external
     view
--- contracts/contracts/reputation/Forge.sol
+++ contracts/contracts/reputation/Forge.sol
@@ -6,7 +6,7 @@ import "../interfaces/IWands.sol";
 import "../interfaces/IForge.sol";
 
 contract Forge is Ownable, IForge {
-  mapping(address => uint256) public XP; // account to XP
+  mapping(address => Character) public characters; // account {XP, XP_Spent}
   mapping(uint256 => uint8) public iLvl; // portal to iLvl
   uint256 public maxLevel;
   IWands public wand;
@@ -16,16 +16,21 @@ contract Forge is Ownable, IForge {
   }
 
   // increases a portals iLvl irrevocably
+  // XP per account can only be assigned to one portal NFT
   // warning this lets new holders downgrade the iLvl
-  function forgePortal(uint256 _id) public {
+  function forgePortal(uint256 _id, uint256 _amount) public {
     require(wand.ownerOf(_id) == msg.sender);
+    Character memory temp = characters[msg.sender];
+    // ensure that this account has XP left to assign
+    require((temp.XP - temp.XPAssigned) > 0);
+    require((temp.XP - temp.XPAssigned) >= _amount);
+    characters[msg.sender].XPAssigned += _amount;
 
-    uint256 _xp = XP[msg.sender];
-    if(_xp > 1000) {
+    if(temp.XP > 1000) {
       iLvl[_id] = 1;
-    } else if(_xp > 5000) {
+    } else if(temp.XP > 5000) {
       iLvl[_id] = 2;
-    } else if (_xp > 10000) {
+    } else if (temp.XP > 10000) {
       iLvl[_id] = 3;
     } else {
       iLvl[_id] = 0;
@@ -37,14 +42,14 @@ contract Forge is Ownable, IForge {
   }
 
   function adjustXP(address _id, uint256 _xp) public onlyOwner {
-    XP[_id] = _xp;
+    characters[_id].XP = _xp;
   }
 
   function adjustXPBatch(address[] memory _ids, uint256[] memory _xps) external onlyOwner {
     require(_ids.length == _xps.length);
 
     for(uint256 i=0; i<= _ids.length;  i++) {
-      XP[_ids[i]] = _xps[i];
+      characters[_ids[i]].XP = _xps[i];
     }  
   }
 
