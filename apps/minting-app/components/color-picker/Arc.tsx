9beafe2039b1522bacf7638bba090aff48039e25
cristovaoth <cristovao.honorato@gmail.com>
2022-07-28 23:35:03 +0100

    Reintroduce useDragRotate. Left HueArc with DragRotate, and converted LightnessArc for style comparion

--- apps/minting-app/components/color-picker/Arc.tsx
+++ apps/minting-app/components/color-picker/Arc.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import classNames from "classnames";
 import { HueGradient, LightnessGradient } from "./Gradient";
 import DragRotate from "../DragRotate";
+import useDragRotate from "../useDragRotate";
 import styles from "./ColorPicker.module.css";
 
 const SIZE = 1000;
@@ -47,51 +48,53 @@ export const HueArc = ({ value, onChange }: Props) => (
   </DragRotate>
 );
 
-export const LightnessArc = ({ value, onChange }: Props) => (
-  <DragRotate value={value} onDragEnd={onChange}>
-    {({ bind, rotation, hovering, dragging }) => (
-      <g
-        transform={`rotate(${rotation}, ${SIZE / 2}, ${SIZE / 2})`}
-        className={classNames(styles.dragGroup, {
-          [styles.hovering]: hovering,
+export const LightnessArc = ({ value, onChange }: Props) => {
+  const { bind, rotation, hovering, dragging } = useDragRotate<SVGPathElement>(
+    value,
+    onChange
+  );
+  return (
+    <g
+      transform={`rotate(${rotation}, ${SIZE / 2}, ${SIZE / 2})`}
+      className={classNames(styles.dragGroup, {
+        [styles.hovering]: hovering,
+      })}
+    >
+      <LightnessGradient />
+      <path
+        fill="none"
+        stroke={"url(#lightness-arc-gray-gradient)"}
+        strokeWidth={40}
+        d={LIGHTNESS_D}
+        className={classNames(styles.grabbableArc)}
+      />
+      <path
+        {...bind()}
+        fill="none"
+        stroke={"rgba(0,0,0,0)"}
+        strokeWidth={85}
+        d={LIGHTNESS_D}
+        className={classNames(styles.grabbableArc, {
+          [styles.active]: dragging,
         })}
-      >
-        <LightnessGradient />
-        <path
-          fill="none"
-          stroke={"url(#lightness-arc-gray-gradient)"}
-          strokeWidth={40}
-          d={LIGHTNESS_D}
-          className={classNames(styles.grabbableArc)}
-        />
-        <path
-          {...bind()}
-          fill="none"
-          stroke={"rgba(0,0,0,0)"}
-          strokeWidth={85}
-          d={LIGHTNESS_D}
-          className={classNames(styles.grabbableArc, {
-            [styles.active]: dragging,
-          })}
-          style={{
-            transform: "scale(1)",
-            transformBox: "fill-box",
-            transformOrigin: "center",
-          }}
-        />
-        <circle
-          cx="500"
-          cy="500"
-          r="250"
-          fill="none"
-          strokeWidth="40"
-          strokeDasharray="5 5"
-          className={styles.lightnessKnurl}
-        />
-      </g>
-    )}
-  </DragRotate>
-);
+        style={{
+          transform: "scale(1)",
+          transformBox: "fill-box",
+          transformOrigin: "center",
+        }}
+      />
+      <circle
+        cx="500"
+        cy="500"
+        r="250"
+        fill="none"
+        strokeWidth="40"
+        strokeDasharray="5 5"
+        className={styles.lightnessKnurl}
+      />
+    </g>
+  );
+};
 
 const HUE_D = arc(SIZE, HUE_MARGIN);
 const LIGHTNESS_D = arc(SIZE, LIGHTNESS_MARGIN);
