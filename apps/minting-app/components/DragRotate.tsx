03ba8e33017f650caec87f04af4532b9fc50f81b
cristovaoth <cristovao.honorato@gmail.com>
2022-07-28 12:10:02 +0100

    Keep using render children version of DragRotate. Leave hook in the code

--- apps/minting-app/components/DragRotate.tsx
+++ apps/minting-app/components/DragRotate.tsx
@@ -55,3 +55,43 @@ const DragRotate: React.FC<Props> = ({ children, value = 0, onDragEnd }) => {
 };
 
 export default DragRotate;
+
+export const useDragRotate = (value: number = 0) => {
+  const container = useRef<HTMLDivElement | null>(null);
+
+  const [{ isFirst, isLast, pin, rotation }, set] = useState<{
+    isFirst: boolean;
+    isLast: boolean;
+    pin: number;
+    rotation: number;
+  }>({ isFirst: false, isLast: false, pin: 0, rotation: value });
+
+  const bind = useDrag(
+    ({ first, last, initial: [initialX, initialY], xy: [x, y] }) => {
+      const { center } = dimensions(
+        container.current?.getBoundingClientRect() as DOMRect
+      );
+
+      const delta = clockwiseDelta(
+        //drag start angle
+        toAngle(center, { x: initialX, y: initialY }),
+        //drag current angle
+        toAngle(center, { x, y })
+      );
+
+      set({
+        isFirst: first,
+        isLast: last,
+        pin: first ? rotation : pin,
+        rotation: first ? rotation : (pin + delta) % 360,
+      });
+    }
+  );
+
+  return {
+    bind: () => ({ ...bind(), ref: container }),
+    isFirst,
+    isLast,
+    rotation,
+  };
+};
