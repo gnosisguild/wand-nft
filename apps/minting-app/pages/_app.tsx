40e093a99643cf7fd3861f56d2ec2d2bf9de0b6b
cristovaoth <cristovao.honorato@gmail.com>
2022-08-05 15:07:33 +0200

    First rough pass at connecting the app with Rinkeby: - Introduced an unstyled network switch - Introduced write hooks for the mint function - Theres still no failure/success/loading indicators

--- apps/minting-app/pages/_app.tsx
+++ apps/minting-app/pages/_app.tsx
@@ -2,10 +2,41 @@ import "../styles/globals.css";
 import { AppWrapper } from "../state";
 import type { AppProps } from "next/app";
 
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import { customTheme, BlockieAvatar } from "../components/ConnectButton";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.rinkeby],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_KEY }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Zodiac NFT",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AppWrapper>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          chains={chains}
+          theme={customTheme}
+          avatar={BlockieAvatar}
+        >
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </AppWrapper>
   );
 }
