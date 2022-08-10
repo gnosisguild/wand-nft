b9bf38e4fbdf9ad013a7a1bb2f3b42392c649582
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-08-10 13:21:07 +0200

    memoize astrology calculations

--- apps/minting-app/components/SvgTemplate.tsx
+++ apps/minting-app/components/SvgTemplate.tsx
@@ -1,6 +1,6 @@
 import { constants } from "ethers";
 import { keccak256 } from "ethers/lib/utils";
-import React from "react";
+import React, { useMemo } from "react";
 import { useAccount } from "wagmi";
 import { useAppContext } from "../state";
 import {
@@ -14,23 +14,55 @@ import {
   scalePlanets,
   xp,
 } from "../template";
-import { AppState, TemplateInput } from "../types";
+import { TemplateInput } from "../types";
 import classes from "./SvgTemplate.module.css";
 const template = require("../../../svg/template.svg.hbs");
 
 interface Props {
   input: TemplateInput;
 }
+
 const SvgTemplate: React.FC<Props> = (props) => {
   const { state } = useAppContext();
   const { address = constants.AddressZero } = useAccount();
-  const input = deriveTemplateInput(state, address);
+
+  const planets = useMemo(
+    () =>
+      scalePlanets(
+        calculatePlanets(state.latitude, state.longitude, 0, new Date())
+      ),
+    [state.latitude, state.longitude]
+  );
+  const aspects = useMemo(
+    () =>
+      scaleAspects(
+        calculateAspects(state.latitude, state.longitude, 0, new Date())
+      ),
+    [state.latitude, state.longitude]
+  );
+
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: template(input, {
-          allowProtoMethodsByDefault: true,
-          allowProtoPropertiesByDefault: true,
+        __html: template({
+          planets,
+          aspects,
+          halo: generateHalo(
+            state.halo.shape,
+            state.halo.rhythm,
+            state.background.color.hue
+          ),
+          frame: {
+            level1: true,
+            title: "",
+          },
+          background: state.background,
+          filterLayers,
+          sparkles: [],
+          seed: parseInt(keccak256(address).slice(-4), 16),
+          stone: interpolateStone(state.stone),
+          xp,
+          handle: generateHandle(state.handle),
         }),
       }}
       className={classes.container}
@@ -39,31 +71,3 @@ const SvgTemplate: React.FC<Props> = (props) => {
 };
 
 export default SvgTemplate;
-
-const deriveTemplateInput = (
-  state: AppState,
-  address: string
-): TemplateInput => ({
-  planets: scalePlanets(
-    calculatePlanets(state.latitude, state.longitude, 0, new Date())
-  ),
-  aspects: scaleAspects(
-    calculateAspects(state.latitude, state.longitude, 0, new Date())
-  ),
-  halo: generateHalo(
-    state.halo.shape,
-    state.halo.rhythm,
-    state.background.color.hue
-  ),
-  frame: {
-    level1: true,
-    title: "",
-  },
-  background: state.background,
-  filterLayers,
-  sparkles: [],
-  seed: parseInt(keccak256(address).slice(-4), 16),
-  stone: interpolateStone(state.stone),
-  xp,
-  handle: generateHandle(state.handle),
-});
