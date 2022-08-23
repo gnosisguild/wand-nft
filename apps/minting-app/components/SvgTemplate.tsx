2c5246ba7ae19e1efc7106af6f157d8c939ee65c
cristovaoth <cristovao.honorato@gmail.com>
2022-08-23 12:23:24 +0200

    very first crack at fixing ssr mismatch y Please enter the commit message for your changes. Lines starting

--- apps/minting-app/components/SvgTemplate.tsx
+++ apps/minting-app/components/SvgTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAppContext } from "../state";
 import { transformForRendering } from "../state/transforms/forRendering";
 
--- apps/minting-app/state/AppContext.tsx
+++ apps/minting-app/state/AppContext.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, createContext, useContext, useReducer } from "react";
+import React, {
+  useEffect,
+  createContext,
+  useContext,
+  useReducer,
+  useState,
+} from "react";
 
 import { AppReducer, initialState, Action } from "./AppReducer";
 import { AppState } from "../types";
@@ -9,33 +15,58 @@ const AppContext = createContext<{
 }>({ state: initialState, dispatch: () => {} });
 
 export function AppWrapper({ children }: { children: React.ReactNode }) {
-  const [state, dispatch] = useReducer(
-    AppReducer,
-    initialState,
-    (initialState: AppState): AppState => {
-      let existingState;
-      if (typeof window !== "undefined") {
-        const storedState = localStorage.getItem("appState");
-        if (storedState) {
-          existingState = JSON.parse(storedState);
-        }
-      }
-      return existingState ? existingState : initialState;
-    }
-  );
-
-  const contextValue = { state, dispatch };
+  const [isClient, setIsClient] = useState(false);
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
   useEffect(() => {
-    if (state && state !== initialState) {
-      localStorage.setItem("state", JSON.stringify(state));
-    }
-  }, [state]);
+    // https://nextjs.org/docs/messages/react-hydration-error#possible-ways-to-fix-it
+    setIsClient(true);
+  }, []);
 
   return (
-    <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+    <AppContext.Provider
+      value={{ state: isClient ? state : ssrState, dispatch }}
+    >
+      {children}
+    </AppContext.Provider>
   );
 }
 export function useAppContext() {
   return useContext(AppContext);
 }
+
+export const ssrState: AppState = {
+  minting: false,
+  background: {
+    radial: true,
+    dark: true,
+    color: {
+      hue: 281,
+      saturation: 33,
+      lightness: 40,
+    },
+  },
+  halo: {
+    shape: 3,
+    rhythm: [
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+      false,
+      true,
+    ],
+  },
+  handle: 0,
+  stone: 0,
+  tokenId: 0,
+  latitude: 0,
+  longitude: 0,
+};
