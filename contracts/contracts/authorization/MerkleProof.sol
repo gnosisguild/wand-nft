d758e7b010501d316adb0460d23336a7c912fd8c
cristovaoth <cristovao.honorato@gmail.com>
2022-09-02 12:26:17 +0100

    Minimal MerkleProof implementation. Tests

---
+++ contracts/contracts/authorization/MerkleProof.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+library MerkleProof {
+  function verify(
+    bytes32[] calldata proof,
+    bytes32 root,
+    bytes32 leaf
+  ) internal pure returns (bool) {
+    bytes32 computed = leaf;
+    for (uint256 i = 0; i < proof.length; i++) {
+      computed = hashPair(computed, proof[i]);
+    }
+    return computed == root;
+  }
+
+  function hashPair(bytes32 a, bytes32 b) private pure returns (bytes32 value) {
+    (a, b) = (a < b) ? (a, b) : (b, a);
+
+    /// @solidity memory-safe-assembly
+    assembly {
+      mstore(0x00, a)
+      mstore(0x20, b)
+      value := keccak256(0x00, 0x40)
+    }
+  }
+}
