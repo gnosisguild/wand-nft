408394b0297a533d48526433d68fbf159bed4592
cristovaoth <cristovao.honorato@gmail.com>
2022-09-06 12:57:03 +0100

    Tasks for cleaning and populating the proof database (spreadsheet)

---
+++ contracts/src/tasks/proofdb.ts
@@ -0,0 +1,158 @@
+import assert from "assert";
+import { ethers } from "ethers";
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import MerkleTree from "merkletreejs";
+
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+
+task(
+  "proofdb:clean",
+  "Recalculates the current MerkleTree and posts root hash to owner safe"
+).setAction(async (_, hre: HardhatRuntimeEnvironment) => {
+  const rows = await resolveENSNames(await readSpreadsheet());
+  await clean(rows);
+});
+
+task(
+  "proofdb:populate",
+  "Recalculates the current MerkleTree and posts root hash to owner safe"
+).setAction(async (_, hre: HardhatRuntimeEnvironment) => {
+  const rows = await resolveENSNames(await readSpreadsheet());
+  const merkleTree = createMerkleTree(rows);
+  await populate(rows, merkleTree);
+});
+
+type Row = {
+  index: number;
+  phrase: string;
+  address?: string;
+  leaf?: string;
+  proof?: string;
+};
+
+export async function readSpreadsheet(): Promise<Row[]> {
+  const sheet = await loadSheet();
+  const rows = await sheet.getRows();
+
+  return rows.map((row: any) => ({
+    index: row._rowNumber - 1,
+    phrase: row.phrase,
+    address: row.address,
+  }));
+}
+
+export async function populate(rows: Row[], merkleTree: MerkleTree) {
+  const sheet = await loadSheet();
+  const { leafColumnIndex, proofColumnIndex } = await columnIndices(
+    sheet,
+    rows
+  );
+
+  for (let i = 0; i < rows.length; i++) {
+    if (i > 0 && i % 100 === 0) {
+      console.info(`Wand Spreasheet: Editing cells from ${i} rows...`);
+    }
+
+    const { index, address, phrase } = rows[i];
+    const leaf = keccak256(address || phrase);
+    const proof = merkleTree.getHexProof(leaf);
+    assert(
+      proof.length > 0,
+      "Wand Spreadsheet: could not retrieve proof from generated MerkleTree"
+    );
+
+    const leafCell = sheet.getCell(index, leafColumnIndex);
+    const proofCell = sheet.getCell(index, proofColumnIndex);
+    leafCell.value = leaf;
+    proofCell.value = JSON.stringify(proof);
+  }
+  console.info(`Saving updated cells`);
+  await sheet.saveUpdatedCells();
+}
+
+export async function clean(rows: Row[]) {
+  const sheet = await loadSheet();
+  const { leafColumnIndex, proofColumnIndex } = await columnIndices(
+    sheet,
+    rows
+  );
+
+  for (let i = 0; i < rows.length; i++) {
+    if (i > 0 && i % 100 === 0) {
+      console.info(`Wand Spreasheet: Cleanings cells from ${i} rows...`);
+    }
+
+    const { index } = rows[i];
+    const leafCell = sheet.getCell(index, leafColumnIndex);
+    const proofCell = sheet.getCell(index, proofColumnIndex);
+    leafCell.value = "";
+    proofCell.value = "";
+  }
+  console.info(`Saving updated cells`);
+  await sheet.saveUpdatedCells();
+}
+
+export async function resolveENSNames(rows: Row[]) {
+  return rows.map((row) => ({
+    ...row,
+    address: ethers.utils.isAddress(row.address || "")
+      ? row.address
+      : undefined,
+  }));
+}
+
+export function createMerkleTree(rows: Row[]): MerkleTree {
+  const leaves = rows.map((row) => row.address || row.phrase);
+  return new MerkleTree(leaves, keccak256, {
+    sortPairs: true,
+    hashLeaves: true,
+  });
+}
+
+export function keccak256(input: string) {
+  const toBytes = typeof input === "string" && !ethers.utils.isHexString(input);
+
+  return ethers.utils.keccak256(
+    toBytes ? ethers.utils.toUtf8Bytes(input) : input
+  );
+}
+
+async function loadSheet() {
+  const doc = new GoogleSpreadsheet(process.env["SPREADSHEET_ID"]);
+
+  await doc.useServiceAccountAuth({
+    client_email: process.env["GOOGLE_SERVICE_ACCOUNT_EMAIL"],
+    private_key: process.env["GOOGLE_PRIVATE_KEY"]?.replace(/\\n/g, "\n"),
+  });
+  await doc.loadInfo();
+  return doc.sheetsByIndex[0];
+}
+
+async function columnIndices(sheet: any, rows: Row[]) {
+  const remoteRows = await sheet.getRows();
+
+  for (let i = 0; i < rows.length; i++) {
+    assert(
+      rows[i].index === remoteRows[i]._rowNumber - 1 &&
+        rows[i].phrase === remoteRows[i].phrase,
+      "Wand Spreadsheet: read write mismatch"
+    );
+  }
+
+  const leafColumnIndex = sheet.headerValues.indexOf("leaf");
+  const proofColumnIndex = sheet.headerValues.indexOf("proof");
+
+  assert(
+    leafColumnIndex !== -1,
+    "Wand Spreadsheet: couldn't find leaf column index"
+  );
+  assert(
+    proofColumnIndex !== -1,
+    "Wand Spreadsheet: couldn't find proof column index"
+  );
+
+  await sheet.loadCells();
+
+  return { leafColumnIndex, proofColumnIndex };
+}
