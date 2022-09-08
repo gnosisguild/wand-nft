29b2cff0c7642044579b795f12aa92bff8793452
Colin Spence <udappia@gmail.com>
2022-09-08 11:14:02 -0600

    Forces button click before state change handling

--- apps/minting-app/components/Music/index.tsx
+++ apps/minting-app/components/Music/index.tsx
@@ -6,18 +6,35 @@ import Drone from "./Drone";
 
 const Music: React.FC = () => {
   const { state, dispatch } = useAppContext();
+  const [ready, setReady] = useState(false);
   const [play, setPlaying] = useState(false);
 
   useEffect(() => {
-    setPlaying(!play);
-    window.setTimeout(() => setPlaying(true), 100);
+    if (ready) {
+      setPlaying(!play);
+      window.setTimeout(() => setPlaying(true), 100);
+    }
   }, [state]);
+
+  const handleClick = () => {
+    if (ready) {
+      setPlaying(!play);
+    } else {
+      setReady(true);
+    }
+  };
 
   return (
     <>
       <MagicSeq play={play} />
       <Mids play={play} />
       <Drone play={play} />
+      <button
+        style={{ position: "relative", zIndex: 99999 }}
+        onClick={() => handleClick()}
+      >
+        {`${play ? "Pause" : "Play"} Magic`}
+      </button>
     </>
   );
 };
