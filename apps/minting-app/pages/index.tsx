7eeb3771bce9420900c8fb83f6d85fd8de3aee34
Colin Spence <udappia@gmail.com>
2022-09-08 22:32:40 -0600

    Initial commit

--- apps/minting-app/pages/index.tsx
+++ apps/minting-app/pages/index.tsx
@@ -14,7 +14,9 @@ import PickerLabels from "../components/PickerLabels";
 import RecastButton from "../components/IconButton/RecastButton";
 import Layout from "../components/Layout";
 import HandleClock from "../components/HandleClock";
+import Sparkles from "../components/Sparkles";
 import Music from "../components/Music";
+import * as Tone from "tone";
 import {
   FullDownloadButton,
   PFPDownloadButton,
@@ -36,6 +38,7 @@ const Home: NextPage = () => {
       <div className={styles.centerContainer}>
         <CenterGilding className={classNames(mintingClasses)} />
         <PickerLabels className={classNames(mintingClasses, styles.hasSvg)} />
+        {Tone.Transport.state === "started" && <Sparkles />}
         <div
           className={classNames(styles.svgPreview, {
             [styles.mintingPreview]: minting,
