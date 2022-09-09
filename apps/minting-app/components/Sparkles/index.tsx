6ba801d1c2a2683eb6e38dba0149fcc1b341a885
Colin Spence <udappia@gmail.com>
2022-09-08 22:32:40 -0600

    Initial commit

---
+++ apps/minting-app/components/Sparkles/index.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Sparkles.module.css";
+import * as Tone from "tone";
+
+const Sparkle: React.FC = () => {
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const size = Math.random() * 10;
+  const pos = {
+    x: Math.random() * windowDimensions.width * 1.2 - 0.1,
+    y: Math.random() * windowDimensions.height * 1.2 - 0.1,
+  };
+  const move = {
+    x: `${Math.random() * 200 - 100}px`,
+    y: `${Math.random() * 200 - 100}px`,
+  };
+  const opacity = 4 / size;
+  const dur = Math.random() * 60 + 15;
+  const delay = Math.random() * 2;
+  return (
+    <div
+      className={styles.sparkleParent}
+      style={
+        {
+          animationDuration: `${dur}s`,
+          animationDelay: `${delay}s`,
+          "--moveX": `${move.x}`,
+          "--moveY": `${move.y}`,
+        } as React.CSSProperties
+      }
+    >
+      <div
+        className={styles.sparkle}
+        style={
+          {
+            animationDuration: `${dur / 3}s`,
+            animationDelay: `${delay}s`,
+            backgroundColor: `hsl(43,65%,${Math.random() * 60 + 30}%)`,
+            filter: `blur(${size / 4}px)`,
+            height: size,
+            left: pos.x,
+            opacity: 0,
+            top: pos.y,
+            width: size,
+            "--opacity": opacity,
+          } as React.CSSProperties
+        }
+      ></div>
+    </div>
+  );
+};
+
+const Sparkles: React.FC = () => {
+  let frequency;
+  let numSparkles = 250;
+
+  useEffect(() => {
+    frequency = Tone.Transport.bpm;
+  });
+
+  return (
+    <div className={styles.wrapper}>
+      {[...Array(numSparkles)].map((item, index) => (
+        <Sparkle key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default Sparkles;
--- apps/minting-app/pages/index.tsx
+++ apps/minting-app/pages/index.tsx
@@ -14,7 +14,9 @@ import PickerLabels from "../components/PickerLabels";
 import RecastButton from "../components/IconButton/RecastButton";
 import Layout from "../components/Layout";
 import MintingToast from "../components/MintingToast";
+import Sparkles from "../components/Sparkles";
 import Music from "../components/Music";
+import * as Tone from "tone";
 import {
   FullDownloadButton,
   PFPDownloadButton,
@@ -46,6 +48,7 @@ const Home: NextPage = () => {
       <div className={styles.centerContainer}>
         <CenterGilding className={classNames(mintingClasses)} />
         <PickerLabels className={classNames(mintingClasses, styles.hasSvg)} />
+        {Tone.Transport.state === "started" && <Sparkles />}
         <div
           className={classNames(styles.svgPreview, {
             [styles.mintingPreview]: isMinting,
@@ -73,8 +76,7 @@ const Home: NextPage = () => {
         <PFPDownloadButton />
       </div>
 
-    <Music />
-
+      <Music />
     </Layout>
   );
 };
