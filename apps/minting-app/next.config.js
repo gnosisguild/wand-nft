2ceee8c079b1887d29bd4bd7a91e56af1248569e
Colin Spence <udappia@gmail.com>
2022-09-13 12:01:19 -0600

    adds background tracks

--- apps/minting-app/next.config.js
+++ apps/minting-app/next.config.js
@@ -7,7 +7,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config) => {
+  webpack: (config, options) => {
+    const { isServer } = options;
     config.module.rules.push({
       test: /\.hbs/,
       use: [
@@ -25,6 +26,23 @@ const nextConfig = {
                 callback();
               }
             },
+          },
+        },
+      ],
+    },
+    {
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: require.resolve('url-loader'),
+          options: {
+            limit: config.inlineImageLimit,
+            fallback: require.resolve('file-loader'),
+            publicPath: `${config.assetPrefix}/_next/static/images/`,
+            outputPath: `${isServer ? '../' : ''}static/images/`,
+            name: '[name]-[hash].[ext]',
+            esModule: config.esModule || false,
           },
         },
       ],
