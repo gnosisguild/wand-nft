6e7d38bd1927ca3f97161869627ac4c03b91934b
samepant <sampanter@gmail.com>
2022-09-19 13:12:49 -0400

    add helper to already minted modal

--- apps/minting-app/components/MintButton/AlreadyMintedModal.tsx
+++ apps/minting-app/components/MintButton/AlreadyMintedModal.tsx
@@ -11,7 +11,11 @@ const AlreadMintedModal: React.FC<Props> = ({ onClose }) => {
   return (
     <Modal onClose={onClose}>
       <div className={styles.passwordModal}>
-        <p>Already minted</p>
+        <h2>Account already minted.</h2>
+        <div className={styles.passwordHelper}>
+          <p>Want to mint another Wand?</p>
+          <p>Read about how to another incantation here.</p>
+        </div>
       </div>
     </Modal>
   );
