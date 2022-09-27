e723714214fe8f4b17260cc7e676c95781b78c2b
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-09-27 14:05:35 +0200

    fix test setup

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,5 +1,5 @@
 import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
+import "@nomicfoundation/hardhat-toolbox";
 import "solidity-coverage";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
