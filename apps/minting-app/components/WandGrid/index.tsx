515d46ec7cc777557e31b953bb6bde96939f9f01
Colin Spence <udappia@gmail.com>
2022-09-29 13:12:48 -0600

    Adds parchment mask to wand grid

--- apps/minting-app/components/WandGrid/index.tsx
+++ apps/minting-app/components/WandGrid/index.tsx
@@ -8,20 +8,24 @@ type Props = {
 };
 
 const WandGrid: React.FC<Props> = ({ wands }) => (
-  <ul className={styles.wandGrid}>
-    {wands.length > 0 &&
-      wands
-        .filter((uri) => uri !== null)
-        .map((tokenUri, i) => (
-          <li key={i}>
-            <Link href={`/wands/${i}`} passHref>
-              <div className={styles.wandContainer}>
-                <WandImage tokenUri={tokenUri} tokenId={i} width={320} />
-              </div>
-            </Link>
-          </li>
-        ))}
-  </ul>
+  <>
+    <div className={styles.wandGridBorderTop} />
+    <ul className={styles.wandGrid}>
+      {wands.length > 0 &&
+        wands
+          .filter((uri) => uri !== null)
+          .map((tokenUri, i) => (
+            <li key={i}>
+              <Link href={`/wands/${i}`} passHref>
+                <div className={styles.wandContainer}>
+                  <WandImage tokenUri={tokenUri} tokenId={i} width={320} />
+                </div>
+              </Link>
+            </li>
+          ))}
+    </ul>
+    <div className={styles.wandGridBorderBottom} />
+  </>
 );
 
 export default WandGrid;
