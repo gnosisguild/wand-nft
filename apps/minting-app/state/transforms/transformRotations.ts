0ae5ebc1be648da5757c985928b8ec5653bf7a3d
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-09-30 12:36:15 +0200

    accept any angle for lightness

--- apps/minting-app/state/transforms/transformRotations.ts
+++ apps/minting-app/state/transforms/transformRotations.ts
@@ -23,8 +23,11 @@ const LIGHTNESS_BOUNDS = [20, 70];
 
 // map an angle between (0deg: white, 180deg: black, 360deg: white) to a lightness between 20% and 70%
 function toLightness(angle: number): number {
+  let normalizedAngle = angle % 360;
   const [left, right] = LIGHTNESS_BOUNDS;
-  return Math.round(left + ((right - left) * Math.abs(angle - 180)) / 180);
+  return Math.round(
+    left + ((right - left) * Math.abs(normalizedAngle - 180)) / 180
+  );
 }
 
 export function toStoneId(angle: number) {
